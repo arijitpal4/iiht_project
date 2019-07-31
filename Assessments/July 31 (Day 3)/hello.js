443d5064fa58bed8c1bb0f7c49146fc43c491975
arijitpal4 <46805618+arijitpal4@users.noreply.github.com>
2019-07-31 17:01:53 +0530

    Add files via upload

---
+++ Assessments/July 31 (Day 3)/hello.js
@@ -0,0 +1,19 @@
+function sayHello() {
+ alert("Hello World, this is from js file");
+}
+
+function add(a, b)
+{
+var c;
+c = a + b;
+console.log('Can u see this?');
+alert('The result is '+c);
+
+}
+function myFunc(x) {
+  x.style.background = "yellow";
+}
+
+function myFunc2(y) {
+  y.style.background = "white";
+}
